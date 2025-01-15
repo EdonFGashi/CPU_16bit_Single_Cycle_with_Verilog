7292003b375be133f95bb0ffdbfe2afeb7ab8d02
EdonFGashi <133915880+EdonFGashi@users.noreply.github.com>
2025-01-15 20:13:42 +0100

    Create CPU.v
    Adding CPU module code

---
+++ CPU.v
@@ -0,0 +1,31 @@
+	//Hyrje ne CPU - CLock CPU_IN_1
+	module CPU(input Clock);
+
+	//TELAT E BRENDSHEM TE CPU
+      wire [3:0] opcode; //D_OUT_1
+      
+     //CU_OUT_x
+	 wire RegDst, Branch, MemRead, MemWrite, RegWrite, MemToReg, ALUSrc;
+     wire [1:0] ALUOp;
+
+      //inicializimi i Datapath    
+	Datapath DP
+	(Clock,
+	RegDst, Branch, MemRead, MemWrite, RegWrite, MemToReg, ALUSrc,
+	ALUOp, opcode);
+      
+      //Inicializimi i COntrol Unit
+	ControlUnit CU(
+    opcode,
+	RegDst,  
+	ALUSrc,
+    MemToReg,
+    RegWrite,
+	MemRead, 
+	MemWrite, 
+	ALUOp,
+    Branch
+	);
+
+
+    endmodule
