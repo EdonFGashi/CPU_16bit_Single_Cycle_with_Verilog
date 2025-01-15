aa64649fe45bc9a64b8f3e4afa47e48f5e72d7de
EdonFGashi <133915880+EdonFGashi@users.noreply.github.com>
2025-01-15 20:18:46 +0100

    Create Instruction_Memory.v
    Adding Instruction Memory module code

---
+++ Instruction_Memory.v
@@ -0,0 +1,10 @@
+
+module InstructionMemory(input[15:0] PC, output[15:0] Instruction);
+
+  reg[7:0] iMem[127:0];
+
+initial $readmemb("instrMemory.mem", iMem);
+
+  assign Instruction = {iMem[PC], iMem[PC+1]};
+
+endmodule
